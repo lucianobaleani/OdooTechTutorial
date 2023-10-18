6003ea49ae12b056bfec6ee3a415d991b233bb31
lucianobaleani94 <lucianobaleani94@gmail.com>
2023-10-18 15:19:28 -0300

    [UPD][academy]6th Class Content

--- academy/models/course.py
+++ academy/models/course.py
@@ -1,12 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class Course(models.Model):
     _name = "academy.course"
     _description = "Course Info"
 
+    # Reserved Fields
     name = fields.Char(string="Title", required=True)
     active = fields.Boolean(string="Active", default=True)
+
+    # Simple Fields
     description = fields.Text()
     level = fields.Selection(
         string="Level",
@@ -17,3 +21,27 @@ class Course(models.Model):
         ],
         copy=False,
     )
+    session_ids = fields.One2many(
+        comodel_name="academy.session", string="Sessions", inverse_name="course_id"
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        string="Currency",
+        default=lambda self: self.env.company.currency_id.id,
+    )
+    base_price = fields.Monetary(string="Base Price", currency_field="currency_id")
+    additional_fee = fields.Monetary(
+        string="Additional Fee", currency_field="currency_id"
+    )
+    total_price = fields.Monetary(
+        string="Total Price",
+        currency_field="currency_id",
+        compute="_compute_total_price",
+    )
+
+    @api.depends("base_price", "additional_fee")
+    def _compute_total_price(self):
+        for record in self:
+            if record.base_price < 0:
+                raise ValidationError(("Base Price cannot be negative"))
+            record.total_price = record.base_price + record.additional_fee
--- academy/models/session.py
+++ academy/models/session.py
@@ -6,17 +6,30 @@ class Session(models.Model):
     _name = "academy.session"
     _description = "Session Info"
 
-    name = fields.Char(string="Title")
+    name = fields.Char(string="Title", related="course_id.name", readonly="False")
 
     session_number = fields.Char(
         string="Session Number",
         default="S0000",
         copy=False,
         required=True,
-        readonly=True,
+        # readonly=True,
     )
     date_start = fields.Datetime(string="Start  Date", required="True")
     date_end = fields.Datetime("End Date", required=True)
+    course_id = fields.Many2one(
+        comodel_name="academy.course",
+        string="Course",
+        ondelete="cascade",
+        # required=True,
+    )
+    instructor_id = fields.Many2one(
+        comodel_name="res.users",
+        string="Instructor",
+        ondelete="restrict",
+    )
+    student_ids = fields.Many2many(comodel_name="res.partner", string="Students")
+    description = fields.Text(related="course_id.description")
 
     @api.model_create_multi
     def create(self, vals_list):
